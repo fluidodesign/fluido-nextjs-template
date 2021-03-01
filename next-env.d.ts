15fe4534dea33763d497c3e3e41fb64a4ce8eb59
Renato Rodrigues <ren.gsg@gmail.com>
2021-03-01 11:21:09 -0300

    tipos genericos

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare interface FreeObject {
+  [key: string]: any
+}
+
+declare interface TypedObject<T> {
+  [key: string]: T
+}
